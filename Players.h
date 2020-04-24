43b3e0c1daa041137a89b9751402ec9a98f741a5
scarney18 <sean.carney@ucdconnect.ie>
2020-04-24 20:44:28 +0100

    added Players.h

---
+++ Players.h
@@ -0,0 +1,12 @@
+//
+// Created by carne on 24/04/2020.
+//
+
+#ifndef SEASSIGNMENT2_PLAYERS_H
+#define SEASSIGNMENT2_PLAYERS_H
+struct players{
+    char name[20];
+    int colour;
+} player[2];
+void addPlayers();
+#endif //SEASSIGNMENT2_PLAYERS_H
