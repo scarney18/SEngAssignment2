dcc8ca8e49058de8b8797b84a6ffb8e508e62e83
scarney18 <sean.carney@ucdconnect.ie>
2020-04-24 20:53:16 +0100

    added PrintBoard.h

---
+++ PrintBoard.h
@@ -0,0 +1,8 @@
+//
+// Created by carne on 24/04/2020.
+//
+#include "Board.h"
+#ifndef SEASSIGNMENT2_PRINTBOARD_H
+#define SEASSIGNMENT2_PRINTBOARD_H
+void printBoard(boardSpace theBoard[8][8]);
+#endif //SEASSIGNMENT2_PRINTBOARD_H
